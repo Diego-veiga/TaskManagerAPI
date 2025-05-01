a10f126c5c5eb9e0aeb56c0ac47358513da5744e
DiegoVeiga <diegoroberto27@hotmail.com>
2025-05-01 14:03:08 -0300

    tests: create unit tests for validations

---
+++ TaskManager.UnitTests/Validation/CreateTaskValidationTask.cs
@@ -0,0 +1,94 @@
+﻿
+using TaskManager.Application.DTOs;
+using TaskManager.Application.Validations;
+
+namespace TaskManager.UnitTests.Validation
+{
+    public class CreateTaskValidationTask
+    {
+        [Fact]
+        public void CreateTaskValidation_ShouldPass_WhenAllFieldsAreValid()
+        {
+            var createTask = new CreateTask()
+            {
+                Title = "Title Test",
+                ExpectedCompletionDate = DateTime.Now,
+                Description = " Description Test",
+            };
+            var createTaskValidation = new CreateTaskValidation();
+
+            var result = createTaskValidation.Validate(createTask);
+
+            Assert.True(result.IsValid);
+            Assert.Equal(0, result.Errors.Count());
+        }
+
+        [Fact]
+        public void CreateTaskValidation_ShouldFail_WhenTitleIsEmpty()
+        {
+            var createTask = new CreateTask()
+            {
+                ExpectedCompletionDate = DateTime.Now,
+                Description = " Description Test",
+            };
+            var createTaskValidation = new CreateTaskValidation();
+
+            var result = createTaskValidation.Validate(createTask);
+
+            Assert.False(result.IsValid);
+            Assert.Equal("'Title' must not be empty.", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void CreateTaskValidation_ShouldFail_WhenTitleIsTooShort()
+        {
+            var createTask = new CreateTask()
+            {
+                Title = "f",
+                ExpectedCompletionDate = DateTime.Now,
+                Description = " Description Test",
+            };
+            var createTaskValidation = new CreateTaskValidation();
+
+            var result = createTaskValidation.Validate(createTask);
+
+            Assert.False(result.IsValid);
+            Assert.Equal("The length of 'Title' must be at least 3 characters. You entered 1 characters.", result.Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public void CreateTaskValidation_ShouldFail_WhenExpectedCompletionDateIsInThePast()
+        {
+            var createTask = new CreateTask()
+            {
+                Title = "teste025",
+                ExpectedCompletionDate = DateTime.Now.AddDays(-2),
+                Description = " Description Test",
+            };
+            var createTaskValidation = new CreateTaskValidation();
+
+            var result = createTaskValidation.Validate(createTask);
+
+            Assert.False(result.IsValid);
+            Assert.Equal("The date cannot be later than today.", result.Errors[0].ErrorMessage);
+        }
+
+
+
+        [Fact]
+        public void CreateTaskValidation_ShouldFail_WhenDescriptionIsEmpty()
+        {
+            var createTask = new CreateTask()
+            {
+                Title = "teste025",
+                ExpectedCompletionDate = DateTime.Now.AddDays(2),
+            };
+            var createTaskValidation = new CreateTaskValidation();
+
+            var result = createTaskValidation.Validate(createTask);
+
+            Assert.False(result.IsValid);
+            Assert.Equal("'Description' must not be empty.", result.Errors[0].ErrorMessage);
+        }
+    }
+}
---
+++ TaskManager.UnitTests/Validation/UpdateStatusTaskValidationTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using TaskManager.Application.DTOs;
+using TaskManager.Application.Validations;
+
+
+namespace TaskManager.UnitTests.Validation
+{
+    public class UpdateStatusTaskValidationTests
+    {
+        [Fact]
+        public void CreateTaskValidation_ShouldPass_WhenAllFieldsAreValid1()
+        {
+            var updateStatusTask = new UpdateStatusTask()
+            {
+                Id = Guid.NewGuid(),
+                Status = Core.Enums.TaskState.Pending
+            };
+                            
+            var updateStatusTaskValidation = new UpdateStatusTaskValidation();
+
+            var result = updateStatusTaskValidation.Validate(updateStatusTask);
+
+            Assert.True(result.IsValid);
+            Assert.Equal(0, result.Errors.Count());
+        }
+
+        [Fact]
+        public void CreateTaskValidation_ShouldFail_WhenTitleIsEmpty2()
+        {
+           var updateStatusTask = new UpdateStatusTask()
+            {
+                Id = Guid.NewGuid(),
+                Status = 0
+            };
+                            
+            var updateStatusTaskValidation = new UpdateStatusTaskValidation();
+
+            var result = updateStatusTaskValidation.Validate(updateStatusTask);
+
+            Assert.False(result.IsValid);
+            Assert.Equal("Invalid status. Enter 1 for Pending and 2 for Completed", result.Errors[0].ErrorMessage);
+        }
+
+
+    }
+}
