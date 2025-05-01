0dd12c8a0a7b861693e52b3536178fc7dcfa3f89
DiegoVeiga <diegoroberto27@hotmail.com>
2025-05-01 13:30:43 -0300

    tests: create unit tests for TaskMapper

---
+++ TaskManager.UnitTests/Mappers/TaskMapperTests.cs
@@ -0,0 +1,27 @@
+﻿using TaskManager.Application.Mappers;
+using TaskManager.Core.Entities;
+
+namespace TaskManager.UnitTests.Mappers
+{
+    public  class TaskMapperTests
+    {
+        [Fact]
+
+        public void ToViewModel_ShouldMapTaskProperties_Correctly()
+        {
+            var task = new TaskEntity("Task01", "Description01", DateTime.Now);
+
+            var taskViewModel = TaskMapper.ToViewModel(task);
+
+            Assert.NotNull(taskViewModel);
+            Assert.Equal(taskViewModel.Id, taskViewModel.Id);
+            Assert.Equal(taskViewModel.Title, taskViewModel.Title);
+            Assert.Equal(taskViewModel.Description, taskViewModel.Description);
+            Assert.Equal(taskViewModel.Status, taskViewModel.Status);
+            Assert.Equal(taskViewModel.Active, taskViewModel.Active);
+            Assert.Equal(taskViewModel.ExpectedCompletionDate, taskViewModel.ExpectedCompletionDate);
+            Assert.Equal(taskViewModel.CreatedAt, taskViewModel.CreatedAt);
+            Assert.Equal(taskViewModel.UpdatedAt, taskViewModel.UpdatedAt);
+        }
+    }
+}
